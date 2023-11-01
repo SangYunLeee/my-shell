8c5015c958fbeaacd4c8a3ac09c9d07fd2ecc5e0
sangyun lee <sororiri@gmail.com>
2023-11-01 13:59:17 +0900

    feat: 개인 쉘 스크립트

---
+++ 1_zsh_customs.sh
@@ -0,0 +1,36 @@
+#ADD PATH FOR AUTO_KEY
+export PATH=$HOME/.local/bin:$PATH
+
+#ADD PATH FOR BREW
+export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+
+#ZSH
+plugins=(
+  git
+  autojump
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  kube-ps1
+)
+
+export ZSH_THEME="powerlevel10k/powerlevel10k"
+export ZSH=$HOME/.oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+
+# 현재 스크립트 파일의 디렉토리 경로를 얻기
+SCRIPT_DIR=$(dirname $0)
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f $SCRIPT_DIR/p10k.zsh ]] || source $SCRIPT_DIR/p10k.zsh
+
+# don't put duplicate lines or lines starting with space in the  history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+#shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=200000
+
+export LESS="-XFR"
